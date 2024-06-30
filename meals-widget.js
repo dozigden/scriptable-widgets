1d41b9270d2d3f4a41f5ae9104582ac368578845
dozigden <luke@savageconclave.com>
2024-06-30 10:12:08 +0100

    Icon updates

--- meals-widget.js
+++ meals-widget.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: light-gray; icon-glyph: magic;
+// icon-color: orange; icon-glyph: utensils;
 
 // note, app deep linking urls in format:
 // homeassistant://navigate/lovelace/garden
